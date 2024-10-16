bb3af12c2d8a83a5f3082940a4dd5d60dd15b61f
kimmeh <mhko0411@gmail.com>
2024-10-16 21:27:04 +0900

    feat: Add Main Open Graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,16 @@ import Footer from "@/components/layout/footer";
 export const metadata: Metadata = {
   title: "keepmhwn",
   description: "My Personal Homepage",
+  openGraph: {
+    type: "website",
+    locale: "ko-KR",
+    siteName: "Keepmhwn",
+    description: "Personal Blog",
+    images: {
+      url: "/images/blog-thumbnail.png",
+      alt: "Keepmhwn",
+    },
+  },
 };
 
 export default function RootLayout({
