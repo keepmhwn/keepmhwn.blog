2b3744e2e5ef1597a1d72f4affb332f6846979d9
kimmeh <mhko0411@gmail.com>
2024-10-18 20:58:49 +0900

    fix: blog header image

--- src/components/article/detail/header.tsx
+++ src/components/article/detail/header.tsx
@@ -1,6 +1,7 @@
 import type { MarkdownMetadata } from "@/types/blog";
 
-import { Heading, Image, Text, Stack } from "@chakra-ui/react";
+import Image from "next/image";
+import { Box, Heading, Text, Stack } from "@chakra-ui/react";
 
 import dayjs from "dayjs";
 
@@ -11,12 +12,21 @@ type Props = {
 const ArticleHeader = ({ metadata }: Props) => {
   return (
     <Stack spacing="16px">
-      <Image
-        borderRadius="8px"
-        objectFit="cover"
-        alt={`${metadata.title}-thumbnail`}
-        src={metadata.thumbnail}
-      />
+      <Box
+        sx={{ width: "100%", position: "relative" }}
+        height={{ base: "150px", sm: "200px", md: "300px" }}
+      >
+        <Image
+          fill
+          objectFit="cover"
+          quality={30}
+          alt={`${metadata.title}-thumbnail`}
+          src={metadata.thumbnail}
+          style={{
+            borderRadius: "8px",
+          }}
+        />
+      </Box>
       <Heading color="gray.700">{metadata.title}</Heading>
       <Text color="gray.700">
         {dayjs(metadata.date).format("YYYY년 MM월 DD일")}
